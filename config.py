3d0166f4bfc15fa3e7f87c4d16ce874a0f184f26
cruxifeedsy <cruxifeedsyc@gmail.com>
2026-02-07 17:33:04 +0100

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-‎# Telegram
-‎BOT_TOKEN = "8133705665:AAFhhc0_mx4Cw6Tq72MyP7l4MQEowxCJzzU"
-‎
-‎# MetaTrader 5 credentials
-‎MT5_LOGIN = 213809274          # your demo account number
-‎MT5_PASSWORD = "TkO_7xjr"
-‎MT5_SERVER = "OctaFX-Demo"  # e.g., "ICMarkets-Demo"
